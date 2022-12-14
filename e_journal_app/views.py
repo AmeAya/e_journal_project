c7f8b173dfbd30d8c8f5af2cfcfa8193c410f442
AmeAya <ameameameaya@gmail.com>
2022-12-14 20:59:25 +0600

    Get user type

--- e_journal_app/views.py
+++ e_journal_app/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import *
 
 # Create your views here.
+def homeView(request):
+    thisUser = request.user
+    try:
+        thisStudent = Student.objects.get(user_id=thisUser) # get data from table Student
+        thisUserType = 'Student'
+    except:
+        thisUserType = 'Teacher'
+    return render(request,
+                  template_name='home.html',
+                  context={
+                      'type': thisUserType
+                  }) # context - datas to show on template
