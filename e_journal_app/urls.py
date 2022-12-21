b3d6d3579aac4675cd79fc0850b6a8bafeebd65b
AmeAya <ameameameaya@gmail.com>
2022-12-21 21:18:05 +0600

    Show new subjects

--- e_journal_app/urls.py
+++ e_journal_app/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('student_cabinet/', studentCabinetView, name='student_cabinet'),
     path('students/', allStudentsList, name='all_students'),
     path('student_change_info/', changeStudentView, name='change_student'),
+    path('add_new_subjects/', teacherAddSubjectsView, name='add_subjects'),
     path('', homeView, name='home'),
 ]
