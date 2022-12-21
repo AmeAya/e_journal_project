84fba66da76a7eb33e5d81ad17810214b61a5ffb
AmeAya <ameameameaya@gmail.com>
2022-12-21 20:50:11 +0600

    Student can change his info

--- e_journal_app/urls.py
+++ e_journal_app/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('cabinet/', cabinetView, name='cabinet'),
     path('student_cabinet/', studentCabinetView, name='student_cabinet'),
     path('students/', allStudentsList, name='all_students'),
+    path('student_change_info/', changeStudentView, name='change_student'),
     path('', homeView, name='home'),
 ]
