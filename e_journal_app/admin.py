5faf14300c1c4f1f9e9d06f3bdfd66da924c155b
AmeAya <ameameameaya@gmail.com>
2022-12-28 20:18:40 +0600

    add hyperlink to group in student, add filter horizonal

--- e_journal_app/admin.py
+++ e_journal_app/admin.py
@@ -1,17 +1,31 @@
 from django.contrib import admin
 from .models import *
+from django import forms
+from django.urls import reverse
+from django.utils.html import urlencode, format_html
 
 # Register your models here.
 admin.site.register(Group)
 admin.site.register(Grade)
 admin.site.register(Exams)
-admin.site.register(Teacher)
 admin.site.register(Attendance)
 admin.site.register(Subject)
 
+class StudentAdminForm(forms.ModelForm):
+    class Meta:
+        model = Student
+        fields = '__all__'
+
+    def clean_name(self):
+        if self.cleaned_data['name'] == 'Banana':
+            raise forms.ValidationError('Banana is not a Name!')
+
+        return self.cleaned_data['name']
+
 @admin.register(Student)
 class StudentAdmin(admin.ModelAdmin):
-    list_display = ('name', 'surname', 'group', 'average_grade')
+    form = StudentAdminForm
+    list_display = ('name', 'surname', 'group_link', 'average_grade')
     list_filter = ('group',)
     search_fields = ('surname__startswith', )
 
@@ -21,12 +35,15 @@ class StudentAdmin(admin.ModelAdmin):
 
     def average_grade(self, obj):
         from django.db.models import Avg
-        from django.utils.html import format_html
         result = Grade.objects.filter(student=obj).aggregate(Avg('points'))
         return format_html('<b>{}</b>', result['points__avg'])
 
-# Только в Админке. В модели не лезть
-# Для Group сделать фильтр по дате поступления
-# Сделать поисковик среди Teacher
-# Добавить колонку в Subject price но в kzt
-# Счиать что 1 кредит в Price равен 22000 KZT
+    def group_link(self, obj):
+        url = (
+            reverse("admin:e_journal_app_group_changelist") + str(obj.group.id)
+        )
+        return format_html("<a href=" + url + ">" + obj.group.name + "</a>")
+
+@admin.register(Teacher)
+class TeacherAdmin(admin.ModelAdmin):
+    filter_horizontal = ('subject',)
